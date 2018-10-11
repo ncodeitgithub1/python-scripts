166282929d55b99843f356d8e4ccfcccab5ddf76
saikumar <ncodeit.github@gmail.com>
2018-10-11 17:03:52 +0530

    added

--- ex1.py
+++ ex1.py
@@ -1,31 +1,91 @@
 import datetime
-myname = 'saikumar'
+myname = 'SAIKUMAR'
 myid = 'NCD0518H024'
 now = datetime.datetime.now()
 
 print  " script executed by %s with id %s " % (myname, myid)
 print now.isoformat()
 
-print "i will now count my chikens:"
-print "Hens", 25 + 30 / 6
-print "Rossters", 100 - 25 * 3 % 4
+# Learn Python The Hard Way. Exercise 41
+import random
+from urllib import urlopen
+import sys
 
-print "how i will count the eggs:"
+WORD_URL = "http://learncodethehardway.org/words.txt"
+WORDS = []
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 
+PHRASES = {
+    "class %%%(%%%):":
+      "Make a class named %%% that is-a %%%.",
+    "class %%%(object):\n\tdef __init__(self, ***)" :
+      "class %%% has-a __init__ that takes self and *** parameters.",
+    "class %%%(object):\n\tdef ***(self, @@@)":
+      "class %%% has-a function named *** that takes self and @@@ parameters.",
+    "*** = %%%()":
+      "Set *** to an instance of class %%%.",
+    "***.***(@@@)":
+      "From *** get the *** function, and call it with parameters self, @@@.",
+    "***.*** = '***'":
+      "From *** get the *** attribute and set it to '***'."
+}
 
-print "is it true that 3 + 2 < 5 - 7?"
+# do they want to drill phrases first
+if len(sys.argv) == 2 and sys.argv[1] == "english":
+    PHRASE_FIRST = True
+else:
+    PHRASE_FIRST = False
 
-print 3 + 2 < 5 - 7
+# load up the words from the website
+for word in urlopen(WORD_URL).readlines():
+    WORDS.append(word.strip())
 
-print "what is 3 + 2?", 3 + 2
 
-print "what is 5 -7?", 5 - 7
+def convert(snippet, phrase):
+    class_names = [w.capitalize() for w in
+                   random.sample(WORDS, snippet.count("%%%"))]
+    other_names = random.sample(WORDS, snippet.count("***"))
+    results = []
+    param_names = []
 
-print "oh, that's why it's False."
+    for i in range(0, snippet.count("@@@")):
+        param_count = random.randint(1,3)
+        param_names.append(', '.join(random.sample(WORDS, param_count)))
 
-print "how about some more."
+    for sentence in snippet, phrase:
+        result = sentence[:]
 
-print "is it greater?", 5 > -2
-print "is it grater or equal", 5 >= -2
-print "is it less or equal?", 5 <= -2
+        # fake class names
+        for word in class_names:
+            result = result.replace("%%%", word, 1)
+
+        # fake other names
+        for word in other_names:
+            result = result.replace("***", word, 1)
+
+        # fake parameter lists
+        for word in param_names:
+            result = result.replace("@@@", word, 1)
+
+        results.append(result)
+
+    return results
+
+
+# keep going until they hit CTRL-D
+try:
+    while True:
+        snippets = PHRASES.keys()
+        random.shuffle(snippets)
+
+        for snippet in snippets:
+            phrase = PHRASES[snippet]
+            question, answer = convert(snippet, phrase)
+            if PHRASE_FIRST:
+                question, answer = answer, question
+
+            print question
+
+            raw_input("> ")
+            print "ANSWER:  %s\n\n" % answer
+except EOFError:
+    print "\nBye"
--- ex2.py
+++ ex2.py
@@ -6,8 +6,21 @@ now = datetime.datetime.now()
 print  " script executed by %s with id %s " % (myname, myid)
 print now.isoformat()
 
+print"i will count chickns:"
+print"hens",25+30/6
+print"roosters",100-25*3%4
 
+print"how will i count the eggs:"
+print 3+2+1-5+4%2-1/4+6
 
+print 3+2<5-7
 
-print "hello world"
-#print "print again"
+print "what is 3+2?",3+2
+
+print "what is 5-7?",5-7
+
+print"how about some more"
+
+print "is it greater?",5>-2
+print "is it graeter or equal?",5>=-2
+print"is it less or equal",5<=-2
